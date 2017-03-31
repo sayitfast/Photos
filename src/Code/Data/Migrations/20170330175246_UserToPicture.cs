2075cb5f85589396bd4a3d99ffeab5a241fad33d
StoyanVitanov <stoyan9701@gmail.com>
2017-03-31 11:08:38 +0300

    Users To Pictures - Fix

---
+++ src/Code/Data/Migrations/20170330175246_UserToPicture.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Code.Migrations
+{
+    public partial class UserToPicture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Images",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Desctiprion",
+                table: "Images",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Images",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_UserId",
+                table: "Images",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_AspNetUsers_UserId",
+                table: "Images",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_AspNetUsers_UserId",
+                table: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_UserId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "Desctiprion",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Images",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Description",
+                table: "Images",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
