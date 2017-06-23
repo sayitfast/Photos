c6091289e0485fab77fd3669c16c92d57ee0fbd9
StoyanVitanov <stoyan9701@gmail.com>
2017-06-23 20:08:17 +0300

    cleaning

--- src/Code/Models/AlbumVIewModels/DeleteAlbumViewModels/DeleteAlbumCommentsViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Code.Models.AlbumVIewModels.DeleteAlbumViewModels
-{
-	using Code.Data;
-
-    public class DeleteAlbumCommentsViewModel
-    {
-		public ApplicationUser User { get; set; }
-    }
-}
